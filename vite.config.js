f3e892b52a5292a3c8d1bae4c493680966cf678d
dmtweedy <damon.tweedy@yahoo.com>
2024-04-01 01:22:00 -0500

    pages pt 6

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/APnetfirst-web",
   plugins: [react()],
   build: {
     rollupOptions: {
