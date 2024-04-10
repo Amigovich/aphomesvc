b3e3e5d75d920d53f111edd1fa396d42290af51d
dmtweedy <damon.tweedy@yahoo.com>
2024-04-09 20:07:11 -0500

    added header styles

--- src/js/Navigation.jsx
+++ src/js/Navigation.jsx
@@ -1,9 +1,10 @@
 // Navigation.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 
 function Navigation({ onItemClick }) {
+    const location = useLocation()
     const handleItemClick = () => {
     // Call the parent component's function to close the navigation menu
     onItemClick();
@@ -12,23 +13,27 @@ function Navigation({ onItemClick }) {
     return (
     <Nav className="ml-auto">
         <Nav.Item>
-        <Link to="/services" className="nav-link" onClick={handleItemClick}>
+        <Link to="/services" className={`nav-link ${location.pathname === '/services' ? 'active' : ''}`} onClick={handleItemClick}>
             Services
+            {location.pathname === '/services' && <div className="active-line1"></div>}
         </Link>
         </Nav.Item>
         <Nav.Item>
-        <Link to="/gallery" className="nav-link" onClick={handleItemClick}>
+        <Link to="/gallery" className={`nav-link ${location.pathname === '/gallery' ? 'active' : ''}`} onClick={handleItemClick}>
             Gallery
+            {location.pathname === '/gallery' && <div className="active-line2"></div>}
         </Link>
         </Nav.Item>
         <Nav.Item>
-        <Link to="/about" className="nav-link" onClick={handleItemClick}>
+        <Link to="/about" className={`nav-link ${location.pathname === '/about' ? 'active' : ''}`} onClick={handleItemClick}>
             About
+            {location.pathname === '/about' && <div className="active-line3"></div>}
         </Link>
         </Nav.Item>
         <Nav.Item>
-        <Link to="/estimate" className="nav-link" onClick={handleItemClick}>
+        <Link to="/estimate" className={`nav-link ${location.pathname === '/estimate' ? 'active' : ''}`} onClick={handleItemClick}>
             Get A Free Quote
+            {location.pathname === '/estimate' && <div className="active-line4"></div>}
         </Link>
         </Nav.Item>
     </Nav>
