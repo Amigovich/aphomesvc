75926c79bdf8be53753c228fb7af2424031c5418
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-05-31 13:55:59 -0500

    Update Header2.jsx
    updated phone number

--- src/js/Header2.jsx
+++ src/js/Header2.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Header2 = () => {
     return (
         <header className="header2">
-            <a className="left">Call Us: 952-212-7499</a>
+            <a className="left">Call Us: 612-208-7271</a>
             <NavLink to="/estimate" className="middle">
                 Click Here to Get A Free Quote
             </NavLink>
