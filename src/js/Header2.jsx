e1df6ce3bf48a8260e03f572fc3a83f00157e5e4
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-01 10:13:00 -0500

    Update Header2.jsx

--- src/js/Header2.jsx
+++ src/js/Header2.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header2 = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://calendar.google.com/calendar/scheduling-button-script.js';
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
         <header className="header2">
             <a className="left">Call Us: 612-208-7271</a>
@@ -9,27 +20,15 @@ const Header2 = () => {
                 Click Here to Get A Free Quote
             </NavLink>
             <a className="right">We Accept Credit Card</a>
-             <a className="right"><!-- Google Calendar Appointment Scheduling begin -->
-<link href="https://calendar.google.com/calendar/scheduling-button-script.css" rel="stylesheet">
-<script src="https://calendar.google.com/calendar/scheduling-button-script.js" async></script>
-<script>
-(function() {
-  var target = document.currentScript;
-  window.addEventListener('load', function() {
-    calendar.schedulingButton.load({
-      url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3bqkNTtzvixRQT-1wc9pMhiAM5lBQMs8XUDpCX_W0ngQe1DCAH0lY67VyQGK5mwvcGr11vW7Vg?gv=true',
-      color: '#D50000',
-      label: "Book Free Consulation Today!",
-      target,
-    });
-  });
-})();
-</script>
-<!-- end Google Calendar Appointment Scheduling --></a>
-
-            
+            <div className="right">
+                <link href="https://calendar.google.com/calendar/scheduling-button-script.css" rel="stylesheet" />
+                <div data-calendar-url="https://calendar.google.com/calendar/appointments/schedules/AcZssZ3bqkNTtzvixRQT-1wc9pMhiAM5lBQMs8XUDpCX_W0ngQe1DCAH0lY67VyQGK5mwvcGr11vW7Vg?gv=true" 
+                     data-calendar-color="#D50000" 
+                     data-calendar-label="Book Free Consultation Today!"></div>
+            </div>
         </header>
     );
-}
+};
 
 export default Header2;
+
