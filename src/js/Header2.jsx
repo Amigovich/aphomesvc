cb8ac16b16dd7a622bcddbbf0e0cb7d87c336292
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-01 10:00:03 -0500

    Update Header2.jsx

--- src/js/Header2.jsx
+++ src/js/Header2.jsx
@@ -9,6 +9,25 @@ const Header2 = () => {
                 Click Here to Get A Free Quote
             </NavLink>
             <a className="right">We Accept Credit Card</a>
+             <a className="right"><!-- Google Calendar Appointment Scheduling begin -->
+<link href="https://calendar.google.com/calendar/scheduling-button-script.css" rel="stylesheet">
+<script src="https://calendar.google.com/calendar/scheduling-button-script.js" async></script>
+<script>
+(function() {
+  var target = document.currentScript;
+  window.addEventListener('load', function() {
+    calendar.schedulingButton.load({
+      url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3bqkNTtzvixRQT-1wc9pMhiAM5lBQMs8XUDpCX_W0ngQe1DCAH0lY67VyQGK5mwvcGr11vW7Vg?gv=true',
+      color: '#D50000',
+      label: "Book Free Consulation Today!",
+      target,
+    });
+  });
+})();
+</script>
+<!-- end Google Calendar Appointment Scheduling --></a>
+
+            
         </header>
     );
 }
