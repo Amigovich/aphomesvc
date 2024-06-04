9b26489ea2ceb556f5b5b35aeb7c4d6b5fb86c33
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-04 12:43:30 -0500

    Update Services.jsx

--- src/js/Services.jsx
+++ src/js/Services.jsx
@@ -8,22 +8,22 @@ const Services = () => {
       <h1 className="text-center">Our Services</h1>
       <p className="text-center">We offer a range of home improvement services, including replacing windows/doors, and general contracting. Our skilled team is committed to delivering top-notch craftsmanship and exceeding customer expectations. Your home is our priority, and we take pride in making your home improvement experience seamless and enjoyable.</p>
       <Row>
-        <Col md={6} lg={6} className="mb-3 mt-5">
-          <Link to="/doors" className="text-decoration-none">
-            <Card className="svc-card">
-              <Card.Body className="svc-card2">
-                <Card.Title className='h5stuff text-center' style= {{ color:"white" }}>Replacement Doors</Card.Title>
-                <Card.Text className='text-center' style= {{ color:"lightgray" }}>Enhance the security and the style of your home with high-quality replacement doors.</Card.Text>
-              </Card.Body>
-            </Card>
-          </Link>
-        </Col>
         <Col md={6} lg={6} className="col2 mb-3 mt-5">
           <Link to="/windows" className="text-decoration-none">
             <Card className="svc-card">
               <Card.Body className="svc-card2">
                 <Card.Title className='h5stuff text-center' style= {{ color:"white" }}>Replacement Windows</Card.Title>
                 <Card.Text className='text-center' style= {{ color:"lightgray" }}>Upgrade your home with energy-efficient windows that enhance curb appeal and increase comfort.</Card.Text>
+              </Card.Body>
+            </Card>
+          </Link>
+        </Col>
+        <Col md={6} lg={6} className="mb-3 mt-5">
+          <Link to="/doors" className="text-decoration-none">
+            <Card className="svc-card">
+              <Card.Body className="svc-card2">
+                <Card.Title className='h5stuff text-center' style= {{ color:"white" }}>Replacement Doors</Card.Title>
+                <Card.Text className='text-center' style= {{ color:"lightgray" }}>Enhance the security and the style of your home with high-quality replacement doors.</Card.Text>
               </Card.Body>
             </Card>
           </Link>
