8b1e73527b3578da5ae5c8c953e399d40c0b5563
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-05 01:05:52 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Header2, Header, Home, Partners, Reviews, About, Estimate, Gallery, Ser
 function App() {
   return (
     <div className="app-container">
-      <BrowserRouter basename="https://aphomesvc.com">
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Header2 />
         <Header />
         <Routes>
