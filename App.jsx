34fd1b5964c0b221a34646a6ed82c4993249566e
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-13 00:33:10 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Header2, Header, Home, Partners, Reviews, About, Estimate, Gallery, Services, Footer, Windows, Doors, Contracting } from './js';
+
+function App() {
+  return (
+    <div className="app-container">
+      <BrowserRouter basename=/aphomesvc/>
+        <Header2 />
+        <Header />
+        <Routes>
+          <Route path='*' element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/home" element={<Home/>} />
+          <Route path='/doors' element={<Doors/>} />
+          <Route path="/windows" element={<Windows/>} />
+          <Route path="/contracting" element={<Contracting/>} />
+          <Route path="/partners" element={<Partners/>} />
+          <Route path="/reviews" element={<Reviews/>} />
+          <Route path="/estimate" element={<Estimate/>} />
+          <Route path="/gallery" element={<Gallery/>} />
+          <Route path="/services" element={<Services/>} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
