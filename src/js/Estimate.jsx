c558ff61a2a9c32a4ad1d2cd628c6a87c0f2f9ad
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-12 20:11:02 -0500

    Update Estimate.jsx

--- src/js/Estimate.jsx
+++ src/js/Estimate.jsx
@@ -16,7 +16,6 @@ function Estimate() {
         backgroundPosition: 'center',
     };
 
-
     function handleSubmit(event) {
         event.preventDefault();
 
@@ -31,7 +30,7 @@ function Estimate() {
         const services = event.target.elements.services.value;
         const additionalServices = event.target.elements.additionalServices.value;
 
-        if (!name || !email || !phone || !address || !services || !additionalServices) {
+        if (!name || !email || !phone || !address || !services) {
             return;
         }
 
@@ -61,7 +60,7 @@ function Estimate() {
             }, 3000);
         })
         .catch((error) => {
-            console.error('Error sending email:', error, 'Please try calling us instead at: ()-');
+            console.error('Error sending email:', error);
         })
         .finally(() => {
             setIsSubmitting(false);
@@ -77,73 +76,73 @@ function Estimate() {
             <p style= {{ color:"lightgray" }}>Interested in financing a project?</p>
             <p style= {{ color:"lightgray" }}>Contact us for a personalized consultation and a free quote today.</p>
             <Form id="contactForm" onSubmit={handleSubmit}>
-    <Row className="mb-4">
-        <Col md={6}>
-        <Form.Group controlId="name">
-            <Form.Label style={{ width: '100%', fontFamily: 'Cuprum', fontSize: '20px', color:"lightgray" }}>Name:</Form.Label>
-            <Form.Control style={{ fontFamily: 'Cuprum' }} type="text" placeholder="Name" required autoComplete="name" />
-        </Form.Group>
-        </Col>
-        <Col md={6}>
-        <Form.Group controlId="email">
-            <Form.Label style={{ width: '100%', fontFamily: 'Cuprum', fontSize: '20px', color:"lightgray"  }}>Email:</Form.Label>
-            <Form.Control style={{ fontFamily: 'Cuprum' }} type="email" placeholder="Email" required autoComplete="email" />
-        </Form.Group>
-        </Col>
-    </Row>
-    <Row className="mb-4">
-        <Col md={6}>
-        <Form.Group controlId="phone">
-            <Form.Label style={{ width: '100%', fontFamily: 'Cuprum', fontSize: '20px', color:"lightgray"  }}>Phone:</Form.Label>
-            <Form.Control style={{ fontFamily: 'Cuprum' }} type="text" placeholder="Phone number" required autoComplete="tel" />
-        </Form.Group>
-        </Col>
-        <Col md={6}>
-        <Form.Group controlId="address">
-            <Form.Label style={{ width: '100%', fontFamily: 'Cuprum', fontSize: '20px', color:"lightgray"  }}>Address:</Form.Label>
-            <Form.Control style={{ fontFamily: 'Cuprum' }} type="text" placeholder="Address" required autoComplete="street-address" />
-        </Form.Group>
-        </Col>
-    </Row>
-    <Row className="mb-4">
-        <Col md={6}>
-        <Form.Group controlId="services">
-            <Form.Label style={{ width: '100%', fontFamily: 'Cuprum', fontSize: '20px', color:"lightgray"  }}>Services Requested:</Form.Label>
-            <Form.Control style={{ fontFamily: 'Cuprum' }} as="select" required autoComplete="off">
-            <option value="windows">Select One</option>
-            <option value="windows">Windows</option>
-            <option value="roofing">Roofing</option>
-            <option value="siding">Siding</option>
-            <option value="gutters">Gutters</option>
-            <option value="doors">Doors</option>
-            <option value="general">General</option>
-            </Form.Control>
-        </Form.Group>
-        </Col>
-        <Col md={6}>
-        <Form.Group controlId="additionalServices">
-            <Form.Label style={{ width: '130%', fontFamily: 'Cuprum', fontSize: '20px', color:"lightgray"  }}>Details and/or Additional Services:</Form.Label>
-            <Form.Control style={{ fontFamily: 'Cuprum' }} type="text" placeholder="Enter additional services" autoComplete="off"/>
-        </Form.Group>
-        </Col>
-    </Row>
-    <Row>
-    <Col md={10}>
-    <Button type="submit" className="sendbtn mt-5" style={{ width: '100%' }}>
-    {isSubmitting ? (
-    <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-    ) : (
-    <span style={{ fontFamily: 'Cuprum', fontSize: '25px' }}>Send Quote Request</span>
-    )}
-    </Button>
-    </Col>
-</Row>
-    {showAlert && (
-        <div className="alert-box alert-success mt-3" role="alert">
-        Message sent successfully!
-        </div>
-    )}
-    </Form>
+                <Row className="mb-4">
+                    <Col md={6}>
+                        <Form.Group controlId="name">
+                            <Form.Label style={{ width: '100%', fontFamily: 'Cuprum', fontSize: '20px', color:"lightgray" }}>Name:</Form.Label>
+                            <Form.Control style={{ fontFamily: 'Cuprum' }} type="text" placeholder="Name" required autoComplete="name" />
+                        </Form.Group>
+                    </Col>
+                    <Col md={6}>
+                        <Form.Group controlId="email">
+                            <Form.Label style={{ width: '100%', fontFamily: 'Cuprum', fontSize: '20px', color:"lightgray"  }}>Email:</Form.Label>
+                            <Form.Control style={{ fontFamily: 'Cuprum' }} type="email" placeholder="Email" required autoComplete="email" />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row className="mb-4">
+                    <Col md={6}>
+                        <Form.Group controlId="phone">
+                            <Form.Label style={{ width: '100%', fontFamily: 'Cuprum', fontSize: '20px', color:"lightgray"  }}>Phone:</Form.Label>
+                            <Form.Control style={{ fontFamily: 'Cuprum' }} type="text" placeholder="Phone number" required autoComplete="tel" />
+                        </Form.Group>
+                    </Col>
+                    <Col md={6}>
+                        <Form.Group controlId="address">
+                            <Form.Label style={{ width: '100%', fontFamily: 'Cuprum', fontSize: '20px', color:"lightgray"  }}>Address:</Form.Label>
+                            <Form.Control style={{ fontFamily: 'Cuprum' }} type="text" placeholder="Address" required autoComplete="street-address" />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row className="mb-4">
+                    <Col md={6}>
+                        <Form.Group controlId="services">
+                            <Form.Label style={{ width: '100%', fontFamily: 'Cuprum', fontSize: '20px', color:"lightgray"  }}>Services Requested:</Form.Label>
+                            <Form.Control style={{ fontFamily: 'Cuprum' }} as="select" required autoComplete="off">
+                                <option value="windows">Select One</option>
+                                <option value="windows">Windows</option>
+                                <option value="roofing">Roofing</option>
+                                <option value="siding">Siding</option>
+                                <option value="gutters">Gutters</option>
+                                <option value="doors">Doors</option>
+                                <option value="general">General</option>
+                            </Form.Control>
+                        </Form.Group>
+                    </Col>
+                    <Col md={6}>
+                        <Form.Group controlId="additionalServices">
+                            <Form.Label style={{ width: '130%', fontFamily: 'Cuprum', fontSize: '20px', color:"lightgray"  }}>Details and/or Additional Services:</Form.Label>
+                            <Form.Control style={{ fontFamily: 'Cuprum' }} type="text" placeholder="Enter additional services" autoComplete="off"/>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={10}>
+                        <Button type="submit" className="sendbtn mt-5" style={{ width: '100%' }}>
+                            {isSubmitting ? (
+                                <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                            ) : (
+                                <span style={{ fontFamily: 'Cuprum', fontSize: '25px' }}>Send Quote Request</span>
+                            )}
+                        </Button>
+                    </Col>
+                </Row>
+                {showAlert && (
+                    <div className="alert-box alert-success mt-3" role="alert">
+                        Message sent successfully!
+                    </div>
+                )}
+            </Form>
         </section>
         </div>
         </div>
