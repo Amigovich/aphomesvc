ca0fa0ccd48b668c025e6ea1f6df52ce36665c1a
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-12 20:27:18 -0500

    Update Estimate.jsx

--- src/js/Estimate.jsx
+++ src/js/Estimate.jsx
@@ -3,7 +3,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 import backgroundImage from '/homesimage.jpg';
 
 function Estimate() {
