30535570bde830d89ba66f46934d6a4d1dbf4450
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-14 16:23:11 -0500

    Update Header2.jsx

--- src/js/Header2.jsx
+++ src/js/Header2.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header2 = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://calendar.google.com/calendar/scheduling-button-script.js';
+        script.async = true;
+        document.body.appendChild(script);
+
+        script.onload = () => {
+            const target = document.getElementById('calendar-scheduling-button');
+            window.calendar.schedulingButton.load({
+                url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3bqkNTtzvixRQT-1wc9pMhiAM5lBQMs8XUDpCX_W0ngQe1DCAH0lY67VyQGK5mwvcGr11vW7Vg?gv=true',
+                color: '#AD1457',
+                label: 'Book an appointment',
+                target,
+            });
+        };
+    }, []);
+
     return (
         <header className="header2">
-            <a className="left">Call Us: 612-208-7271</a>
+            <div className="left">
+                <span>Call Us: 612-208-7271</span>
+                <div id="calendar-scheduling-button" style={{ display: 'inline-block', marginLeft: '10px' }}></div>
+            </div>
             <NavLink to="/estimate" className="middle">
                 Click Here to Get A Free Quote
             </NavLink>
-            <a className="right">We Accept Credit Card</a>
+            <div className="right">We Accept Credit Card</div>
         </header>
     );
 }
