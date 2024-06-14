db8c14592c171e674e884cf41504516cba3fa01b
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-14 10:17:48 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   console.log('App component rendered');
   return (
     <div className="app-container">
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/aphomesvc">
         <Header2 />
         <Header />
         <Routes>
