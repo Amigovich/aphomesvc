ac76784df0aab51faeafdc520ede0caf943915a1
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-14 12:45:00 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://aphomesvc.com/",
+  base: "/",
   build: {
     rollupOptions: {
       external: [],
