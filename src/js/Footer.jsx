1fcf35164bcde4f73ef36d23958fca66f6788cf8
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-14 22:17:37 -0500

    Update Footer.jsx

--- src/js/Footer.jsx
+++ src/js/Footer.jsx
@@ -7,22 +7,36 @@ import logo from '/logo.png';
 function Footer() {
     return (
         <footer className="footer">
-            <Navbar className="footerpadding">
+            <Navbar className="footerpadding" expand="lg">
                 <Nav className="mr-auto">
                     <Navbar.Brand as={Link} to="/home" className="logo-container">
                         <img
-                        src={logo}
-                        width="60px"
-                        height="48px"
-                        className="d-inline-block align-top logo"
-                        alt="AP Home Services Logo"
+                            src={logo}
+                            width="60px"
+                            height="48px"
+                            className="d-inline-block align-top logo"
+                            alt="AP Home Services Logo"
                         />
                     </Navbar.Brand>
                 </Nav>
-                <Nav className="thisone">
-                    <Nav.Link as={Link} to="/estimate" className="foot-cont" style={{ textDecoration: 'none', color: 'white' }}>Contact Us</Nav.Link>
+                <Nav className="ml-auto thisone">
+                    <Nav.Link as={Link} to="/estimate" className="foot-cont" style={{ textDecoration: 'none', color: 'white' }}>
+                        Contact Us
+                    </Nav.Link>
                 </Nav>
-                <Navbar.Text className="ml-auto foot-right" style={{ textDecoration: 'none', color: 'white' }}>Site & Design by&nbsp;<a className="dtlink" style= {{ textDecoration:"none", fontFamily:"Arial", color:"white" }} href="https://www.linkedin.com/in/damon-tweedy-296737291/" target="_blank" rel="noopener noreferrer">DT</a>. &nbsp;&nbsp;&copy; {new Date().getFullYear()} AP Home Services.&nbsp;&nbsp; All rights reserved.&nbsp;&nbsp; MN License #BC807818</Navbar.Text>
+                <Navbar.Text className="ml-auto foot-right" style={{ textDecoration: 'none', color: 'white' }}>
+                    Site & Design by&nbsp;
+                    <a
+                        className="dtlink"
+                        style={{ textDecoration: 'none', fontFamily: 'Arial', color: 'white' }}
+                        href="https://www.linkedin.com/in/damon-tweedy-296737291/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        DT
+                    </a>
+                    . &nbsp;&nbsp;&copy; {new Date().getFullYear()} AP Home Services.&nbsp;&nbsp; All rights reserved.&nbsp;&nbsp; MN License #BC807818
+                </Navbar.Text>
             </Navbar>
         </footer>
     );
