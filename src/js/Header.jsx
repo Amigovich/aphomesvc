b7235c4bf57aae2cf337c05c3372406100ac5564
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-14 22:15:37 -0500

    Update Header.jsx

--- src/js/Header.jsx
+++ src/js/Header.jsx
@@ -13,28 +13,22 @@ function Header() {
   };
 
   const handleToggleClick = () => {
-    // Toggle the expanded state
     setExpanded(!expanded);
-    // Update the showNavLinks state based on the new expanded state
     setShowNavLinks(!expanded);
   };
 
   useEffect(() => {
     const handleResize = () => {
-      // Check if window width is less than or equal to 992px
       if (window.innerWidth <= 992) {
-        // If true, hide nav links when toggler is not expanded
         setShowNavLinks(expanded);
       } else {
-        // If false, always show nav links
         setShowNavLinks(true);
       }
     };
 
-    // Add event listener for window resize
     window.addEventListener('resize', handleResize);
+    handleResize(); // Initial check on mount
 
-    // Cleanup function to remove event listener
     return () => {
       window.removeEventListener('resize', handleResize);
     };
@@ -42,7 +36,7 @@ function Header() {
 
   return (
     <header className="header">
-      <Navbar bg="rgba(0, 0, 0, 0)" expand="lg" expanded={expanded} variant="dark" collapseOnSelect>
+      <Navbar bg="transparent" expand="lg" expanded={expanded} variant="dark" collapseOnSelect>
         <Navbar.Brand as={Link} to="/home" className="logo-container">
           <img
             src={logo}
@@ -55,7 +49,7 @@ function Header() {
         <Navbar.Toggle
           style={{ backgroundColor: 'gray' }}
           aria-controls="responsive-navbar-nav"
-          onClick={handleToggleClick} // Use custom handler for toggler click
+          onClick={handleToggleClick}
         />
         <Navbar.Collapse id="responsive-navbar-nav">
           {showNavLinks && (
