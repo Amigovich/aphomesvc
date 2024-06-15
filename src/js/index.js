d7bc08a2326433313c0c77c887f3bd4596402f20
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-15 08:40:08 -0500

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ export { default as Header } from './Header';
 export { default as Home } from './Home';
 export { default as About } from './About';
 export { default as Estimate } from './Estimate';
+export { default as ContactUs } from './ContactUs';
 export { default as Gallery } from './Gallery';
 export { default as Services } from './Services';
 export { default as Partners } from './Partners';
