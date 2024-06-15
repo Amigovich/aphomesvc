2640582ae21558be96910c3fbab21dd1bc1f503b
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-15 08:34:42 -0500

    Update Footer.jsx

--- src/js/Footer.jsx
+++ src/js/Footer.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
-import logo from '/logo.png'; 
+import logo from '/logo.png';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 function Footer() {
     return (
@@ -11,18 +12,22 @@ function Footer() {
                 <Nav className="mr-auto">
                     <Navbar.Brand as={Link} to="/home" className="logo-container">
                         <img
-                        src={logo}
-                        width="60px"
-                        height="48px"
-                        className="d-inline-block align-top logo"
-                        alt="AP Home Services Logo"
+                            src={logo}
+                            width="60px"
+                            height="48px"
+                            className="d-inline-block align-top logo"
+                            alt="AP Home Services Logo"
                         />
                     </Navbar.Brand>
                 </Nav>
                 <Nav className="thisone">
-                    <Nav.Link as={Link} to="/ContactUs" className="foot-cont" style={{ textDecoration: 'none', color: 'white' }}>Contact Us</Nav.Link>
+                    <Nav.Link as={Link} to="/ContactUs" className="foot-cont" style={{ textDecoration: 'none', color: 'white' }}>
+                        Contact Us
+                    </Nav.Link>
                 </Nav>
-                <Navbar.Text className="ml-auto foot-right" style={{ textDecoration: 'none', color: 'white' }} {new Date().getFullYear()} AP Home Services.&nbsp;&nbsp; All rights reserved.&nbsp;&nbsp; MN License #BC807818</Navbar.Text>
+                <Navbar.Text className="ml-auto foot-right" style={{ textDecoration: 'none', color: 'white' }}>
+                    {new Date().getFullYear()} AP Home Services.&nbsp;&nbsp; All rights reserved.&nbsp;&nbsp; MN License #BC807818
+                </Navbar.Text>
             </Navbar>
         </footer>
     );
