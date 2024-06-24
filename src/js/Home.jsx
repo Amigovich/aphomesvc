f36f6fc21dfcddcfdc5b1928fa31f1399825e40c
Amigovich <161978232+Amigovich@users.noreply.github.com>
2024-06-24 15:53:17 -0500

    Update Home.jsx

--- src/js/Home.jsx
+++ src/js/Home.jsx
@@ -16,7 +16,7 @@ const HomePage = () => {
 
     return (
         <div className="home-page" style={backgroundStyle}>
-            <div className="content-box" style={{ backgroundColor: '#262b3df3', padding: '1px', borderRadius: '30px', maxWidth: '600px', width: '100%' }}>
+            <div className="content-box" style={{ backgroundColor: '#262b3df3', padding: '20px', borderRadius: '10px', maxWidth: '800px', width: '100%' }}>
                 <h1 className="text-center" style={{ color: "white" }}>Welcome to AP Home Services</h1>
                 <p className="text-center" style={{ color: "lightgray" }}>
                     Your premier residential window and door replacement contractor! Ready to elevate your home with new windows and doors? Contact us today to schedule a free consultation. Our friendly and knowledgeable team is here to answer your questions, discuss your options, and provide you with a customized solution that fits your budget and lifestyle. Let's turn your house into the home of your dreams together!
